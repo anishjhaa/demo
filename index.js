61cacfae4aa53c050fb67ddbb09578ea8ebd9c7e
Anish <anishjhacse168@gmail.com>
2025-12-05 11:29:06 +0530

    fourth commit

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const footer = "Footer added";
 const heroSection = "Hero Section added";
 console.log(footer);
 console.log(heroSection);
+
+const about = "About Page added";
+console.log(about);
+
+const contactUs = "Contact Us Page Added";
+console.log(contactUs);
