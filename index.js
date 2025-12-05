3adac2f148d58a3bd36868e7dee4f3aef253eb49
Anish <anishjhacse168@gmail.com>
2025-12-05 11:27:16 +0530

    feature first commit

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const footer = "Footer added";
 const heroSection = "Hero Section added";
 console.log(footer);
 console.log(heroSection);
+
+const mainSection = "Main Section Added";
+console.log(mainSection);
+
+const asideSection = "Aside Section Added";
+console.log(asideSection);
